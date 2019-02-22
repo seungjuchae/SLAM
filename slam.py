3e6fea57ab7b3ae3b401f33618666afdb295d218
seungju <chaeju106@gmail.com>
2019-02-22 02:24:00 +0100

    4. Better ORB extractor
    The circles in the image were being detected , which we want track points of.
    However the circles were not well distributed across image, thus, I wrote better ORB extractor

--- slam.py
+++ slam.py
@@ -1,28 +1,54 @@
 import cv2
 from display import Display
 
-# Width, height of the window
+# Width, height of the window 1920 1080
 (width, height) = (1920//2, 1080//2)
 
 # Window display
 disp = Display(width, height)
-orb = cv2.ORB_create()
-print(dir(orb))
+#orb = cv2.ORB_create()
+#print(dir(orb))
 
+
+class ExtractingFeatures(object):
+    GX = 16//2
+    GY = 16//2
+    def __init__(self):
+        self.orb = cv2.ORB_create(1000)
+    
+    def extract(self, img):
+        # run detect in grid
+        sy = img.shape[0]//self.GY
+        sx = img.shape[1]//self.GX
+        akp = []
+        for ry in range(0, img.shape[0], sy):
+            for rx in range(0, img.shape[1], sx):
+                img_chunk = img[ry:ry+sy, rx:rx+sx]
+                kp = self.orb.detect(img_chunk, None)
+                for p in kp:
+                    p.pt = (p.pt[0] + rx, p.pt[1] + ry)
+                    akp.append(p)
+        return akp
+               
+# ExtractingFeatures
+ex = ExtractingFeatures()
 
 def process_frame(img):
     # resizing the elements in vector
     img = cv2.resize(img, (width, height)) 
+    kp = ex.extract(img)
 
     # https://docs.opencv.org/3.0-beta/modules/features2d/doc/feature_detection_and_description.html
     # keypoint, descriptor
     # https://docs.opencv.org/3.4.3/dc/dc3/tutorial_py_matcher.html
     # this finds the keypoints and descriptors with ORB
-    kp, des = orb.detectAndCompute(img, None)
+    # kp, des = orb.detectAndCompute(img, None)
+
     for p in kp:
         # u,v for coordinate and image
         u,v =map(lambda x: int(round(x)), p.pt)
-        cv2.circle(img, (u,v), color=(0,255,0), radius=5)
+        cv2.circle(img, (u,v), color=(0,255,0), radius=5) 
+  
     disp.paint(img)
     
 
@@ -55,6 +81,6 @@ if __name__ == "__main__":
         if ret == True:
 
             # Display the resulting frame
-            process_frame(frame)
+           process_frame(frame)
         else:
-            break
+           break
